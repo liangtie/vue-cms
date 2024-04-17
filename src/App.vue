fc579c2beba1feb13b5232db40b3c315808972ec
Ethan Chien <liangtie.qian@gmail.com>
2024-04-17 15:36:36 +0800

    Import ecad-viewer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 
-<script src="http://192.168.50.2:8000/ecad-viewer.js"></script>
+<script src="http://localhost:8099/ecad-viewer.js"></script>
 
 <template>
-  <div id="cc"></div>
-  <kc-board-app></kc-board-app>
-  <kc-schematic-app></kc-schematic-app>
+  <div class="flex-container">
+    <ecad-viewer>
+      <ecad-source src="http://localhost:8099/video/video.kicad_pcb"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/bus_pci.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/esvideo.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/graphic.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/modul.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/muxdata.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/pal-ntsc.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/rams.kicad_sch"></ecad-source>
+      <ecad-source src="http://localhost:8099/video/video.kicad_sch"></ecad-source>
+      <ecad-3d-source src="http://localhost:8099/video/video.glb.meshopt.glb"></ecad-3d-source>
+    </ecad-viewer>
+  </div>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+ecad-viewer {
+  width: 100%;
+  height: 800px;
 }
+                        .flex-container {
+                          display: flex;
+                          margin-top : 60px;
+                          margin-left: 10%;
+                          margin-right: 10%;
+                        }
+            
+                        .col-flex-container {
+                          display: flex;
+                          flex-direction: column;
+                        }
+
+                        .canvas-container {
+                          margin: 10px;
+                          padding: 1px;
+                        }
 </style>
