729e8c34657b3257fd5e1d2b2bbdb72a9bd91272
Ethan Chien <liangtie.qian@gmail.com>
2024-04-18 17:20:19 +0800

    Add setup demo

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 
-<script src="http://localhost:8099/ecad-viewer.js"></script>
+
+<script  setup >
+// variable
+const msg = 'Hello!'
+
+
+import  ecad from "http://localhost:8099/ecad-viewer.js"
+
+const urls = [
+  "http://localhost:8099/video/video.kicad_sch",
+  "http://localhost:8099/video/bus_pci.kicad_sch",
+  "http://localhost:8099/video/esvideo.kicad_sch",
+  "http://localhost:8099/video/graphic.kicad_sch",
+  "http://localhost:8099/video/modul.kicad_sch",
+  "http://localhost:8099/video/muxdata.kicad_sch",
+  "http://localhost:8099/video/pal-ntsc.kicad_sch",
+  "http://localhost:8099/video/rams.kicad_sch"
+]
+
+
+function log() {
+  console.log(msg)
+  alert("Button clicked!");
+  console.log(typeof ecad.find_root_sch_by_urls)
+  ecad.find_root_sch_by_urls(urls).then((e) => { alert(`the root sch is ${e}`) })
+
+}
+
+
+</script>
 
 <template>
   <div class="flex-container">
-    <ecad-viewer-embedded url="http://localhost:8099/video/video.kicad_pcb">
-    </ecad-viewer-embedded>
-    <ecad-viewer-embedded url="http://localhost:8099/video/video.glb.meshopt.glb">
-    </ecad-viewer-embedded>
-    <ecad-viewer-embedded url="http://localhost:8099/video/video.kicad_sch;
-      http://localhost:8099/video/bus_pci.kicad_sch;
-      http://localhost:8099/video/esvideo.kicad_sch;
-      http://localhost:8099/video/graphic.kicad_sch;
-      http://localhost:8099/video/modul.kicad_sch;
-      http://localhost:8099/video/muxdata.kicad_sch;
-      http://localhost:8099/video/pal-ntsc.kicad_sch;
-      http://localhost:8099/video/rams.kicad_sch">
-    </ecad-viewer-embedded>
-    <!-- <ecad-viewer>
-      <ecad-source src="http://localhost:8099/video/video.kicad_pcb"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/bus_pci.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/esvideo.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/graphic.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/modul.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/muxdata.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/pal-ntsc.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/rams.kicad_sch"></ecad-source>
-      <ecad-source src="http://localhost:8099/video/video.kicad_sch"></ecad-source>
-      <ecad-3d-source src="http://localhost:8099/video/video.glb.meshopt.glb"></ecad-3d-source>
-    </ecad-viewer> -->
+    <button id="myButton" @click="log">find root sch</button>
   </div>
 
 </template>
@@ -46,7 +51,7 @@ ecad-viewer-embedded {
                           margin-right: 10%;
                                                     flex-direction: column;
                         }
-            
+
                         .col-flex-container {
                           display: flex;
                           flex-direction: column;
